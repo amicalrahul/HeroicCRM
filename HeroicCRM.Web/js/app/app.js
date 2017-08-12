99a20e5e8644d9978d8e13c7e796f2e5f55b2f2b
Rahul Sabharwal <amicalrahul@gmail.com>
2017-08-12 19:55:44 +1000

    Webpack with all dependencies except font-awesome is working
    Pending:
    font-awesome
    minification
    curret stats -
    without webpack => 		    833kb 36 request 1.324s
    with webpack (without minifying) => 980kb 7 request 939ms

--- HeroicCRM.Web/js/app/app.js
+++ HeroicCRM.Web/js/app/app.js
@@ -1,4 +1,9 @@
-﻿
+﻿//require('font-awesome/css/font-awesome.css');
+//require('../../css/app.scss');
+require('bootstrap/dist/css/bootstrap.css');
+require('../../css/sb-admin.css');
+require('../../css/layout.css');
+require('angular-ui-grid/ui-grid.css');
 global.jQuery = require('jquery');
 require('bootstrap');
 require('angular');
@@ -6,13 +11,15 @@ require('angular-animate');
 require('angular-ui-bootstrap/ui-bootstrap');
 require('angular-ui-grid/ui-grid');
 
-require('bootstrap/dist/css/bootstrap.css');
 //require('bootstrap-rtl/dist/css/bootstrap-rtl.css');
-require('../../font-awesome/css/font-awesome.css');
-require('angular-ui-grid/ui-grid.css');
-require('../../css/layout.css');
-require('../../css/sb-admin.css');
-require('../../css/sb-admin-rtl.css');
+//require('font-awesome/css/font-awesome.css');
+//require('../../font-awesome/fonts/fontawesome-webfont.woff');
+//require('../../font-awesome/fonts/fontawesome-webfont.ttf');
+//require('../../fonts/glyphicons-halflings-regular.eot');
+//require('../../fonts/glyphicons-halflings-regular.svg');
+//require('../../fonts/glyphicons-halflings-regular.ttf');
+//require('../../fonts/glyphicons-halflings-regular.woff');
+//require('../../css/sb-admin-rtl.css');
 const ngModule = angular.module('HeroicCRM', ['ngAnimate', 'ui.bootstrap', 'ui.grid']);
 
 require('./authentication')(ngModule);
--- HeroicCRM.Web/webpack.config.js
+++ HeroicCRM.Web/webpack.config.js
@@ -31,14 +31,29 @@ module.exports = {
             test: /\.js$/, exclude: /node_modules/,
             loader: 'babel-loader'
         }, {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file-loader"
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+        }, {
         //    test: /\.ts$/,
         //    loaders: ['awesome-typescript-loader', 'source-map-loader', 'tslint-loader']
         //}, {
-            test: /\.(png|jpg|gif|woff|woff2|ttf|svg|eot)$/,
-            loader: 'url-loader?limit=10000'
+            test: /\.(png|jpg|gif)$/,
+            loader: 'file-loader?limit=10000'
         }, {
             test: /favicon.ico$/, exclude: /node_modules/,
-            loader: 'url-loader?limit=10000'
+            loader: 'file-loader?limit=10000'
         }, {
             test: /\.css$/, 
             loader: 'style-loader!css-loader'
@@ -51,23 +66,24 @@ module.exports = {
         }],
         exprContextCritical: false
     },
-    plugins: [new ExtractTextPlugin('style.css'),
-    new WebpackRTLPlugin()
+    plugins: [
+    //    new ExtractTextPlugin('Built/style.css'),
+    //new WebpackRTLPlugin(),
         //new webpack.optimize.CommonsChunkPlugin({
         //    name: ['app', 'polyfills', /*'vendor'*/]
         //}),
-        //new CleanWebpackPlugin(
-        //    ['./AngularBundle', ]),
+        new CleanWebpackPlugin(
+            ['./built', ]),
         //new HtmlWebpackPlugin({
         //    template: "../Views/Home/",
         //    filename: "./Index.cshtml",
         //    inject: false,
         //})
         //,
-        //new CopyWebpackPlugin([{
-        //    from: './angular2/images/*.*',
-        //    to: 'assets/',
-        //    flatten: true
-        //}])
+        new CopyWebpackPlugin([{
+            from: './angular2/images/*.*',
+            to: 'assets/',
+            flatten: true
+        }])
     ]
 };
